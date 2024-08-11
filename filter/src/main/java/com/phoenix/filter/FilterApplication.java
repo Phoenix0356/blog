a060b61d10437dc2d7ac530e272e43a87fc3a331
Phoenix0356 <1242780667@qq.com>
2024-08-11 14:07:39 +0800

    feature:怎加敏感词过滤服务

--- filter/src/main/java/com/phoenix/filter/FilterApplication.java
+++ filter/src/main/java/com/phoenix/filter/FilterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FilterApplication.class, args);
 	}
